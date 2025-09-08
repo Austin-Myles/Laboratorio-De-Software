c1ec8db03f2dd3867fe70b64b524d92af456f3a4
Austin <88697751+Austin-Myles@users.noreply.github.com>
2025-09-08 20:28:43 -0300

    Update build.bat

--- Practicas/RobocodeLabo-1.10.0/RobocodeLabo/src/laboratorio/build.bat
+++ Practicas/RobocodeLabo-1.10.0/RobocodeLabo/src/laboratorio/build.bat
@@ -9,21 +9,28 @@ cd ..
 
 rem Compilar todos los archivos Java
 echo 📝 Compilando archivos Java...
-javac -cp "../libs/robocode.jar;." laboratorio/*.java estrategias/*.java
+javac -cp "../libs/robocode.jar;." laboratorio/*.java estrategas/*.java estrategias/*.java estrategas/*.java
 
 if %errorlevel% equ 0 (
     echo ✅ Compilación exitosa
+
+    rem Crear directorios de destino
+    mkdir ..\robots\laboratorio
+    mkdir ..\robots\estrategias
+    mkdir ..\robots\estrategas
+
     
     rem Copiar archivos .class a robots/
     echo 📦 Copiando archivos a robots/...
     copy laboratorio\*.class ..\robots\laboratorio\
     copy estrategias\*.class ..\robots\estrategias\
+    copy estrategas\*.class ..\robots\estrategas\
     
     rem Crear/actualizar JAR
     echo 🗃️ Actualizando JAR...
     cd ..\robots
     if exist RobocodeLabo.jar del RobocodeLabo.jar
-    jar cf RobocodeLabo.jar estrategias/*.class laboratorio/*.class laboratorio/*.properties
+    jar cf RobocodeLabo.jar estrategias/*.class laboratorio/*.class laboratorio/*.properties estrategas/*.class
     
     if %errorlevel% equ 0 (
         echo 🏆 ¡Build completado exitosamente!
